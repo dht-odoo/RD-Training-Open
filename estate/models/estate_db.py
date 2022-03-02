c6f643b867eec475b00c36cf2eeb1ac2391fa594
alhu-odoo <alhu@odoo.com>
2022-03-02 12:04:39 +0530

    inverse functionaly added

--- estate/models/estate_db.py
+++ estate/models/estate_db.py
@@ -113,3 +113,20 @@ class TestModel6(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+        readonly=False
+    )
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.today() + relativedelta(
+                days=record.validity
+            )
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = record.date_deadline.day - fields.Date.today().day
